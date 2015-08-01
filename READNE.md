277d2789cdb80ce4a2f7b5bc7b893f82c2e7dd8a
Rusty Geldmacher <russell.geldmacher@gmail.com>
2015-07-31 22:38:02 -0400

    Terminate and re-establish SSL

---
+++ READNE.md
@@ -0,0 +1,5 @@
+To hit the server, go to:
+
+https://a.b.c.d.xip.io
+
+Where `a.b.c.d` is your boot2docker IP.
--- web/server.js
+++ web/server.js
@@ -1,8 +1,22 @@
-var http = require('http');
+var https = require('https'),
+  http = require('http'),
+  fs = require ('fs');
 
-function serve(ip, port)
-{
-  http.createServer(function (req, res) {
+var sslOptions = {
+  key: fs.readFileSync('cert/web.key.pem'),
+  cert: fs.readFileSync('cert/web.crt.pem'),
+  requestCert: false,
+  rejectUnauthorized: true
+};
+
+// Health Check
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('OK');
+}).listen(80, '0.0.0.0');
+
+function serve(ip, port) {
+  https.createServer(sslOptions, function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     var server = process.env.MACHINE + ":" + port;
     res.write("<h2>Hello " + req.headers['x-forwarded-for'] + "!");
@@ -11,4 +25,5 @@ function serve(ip, port)
   }).listen(port, ip);
 }
 
-serve('0.0.0.0', 8080);
+serve('0.0.0.0', 4443);
+serve('0.0.0.0', 4444);
